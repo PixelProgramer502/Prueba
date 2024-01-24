ba255e3f4f37969c75467ae74c0116b3907d8ed0
JoelArriola502 <cdJoelArriola502@gmail.com>
2024-01-23 21:17:06 -0600

    agrege un nuevo mensaje de la rama 2

--- src/js/mensaje.js
+++ src/js/mensaje.js
@@ -1,4 +1,9 @@
 const mensaje=document.getElementById("mensaje");
+const mensaje2=document.getElementById("mensaje2");
 document.addEventListener("DOMContentLoaded",ev=>{
     mensaje.innerHTML="Estoy realizando cambio en esta";
+})
+
+document.addEventListener("DOMContentLoaded",ev=>{
+    mensaje2.innerHTML="Estoy realizando cambio en esta es la rama 2";
 })
